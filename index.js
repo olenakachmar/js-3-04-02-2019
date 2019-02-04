5f7f78e1d3bab7045591338bf0b35424a6145399
Olena Kachmar <olena.kachmar9@gmail.com>
2019-02-04 17:05:15 +0000

    lesson

---
+++ index.js
@@ -0,0 +1,17 @@
+let fordMustang = {};
+fordMustang.color = 'red';
+fordMustang.engine = 'V8 6.0';
+fordMustang.make = 'Ford';
+fordMustang.model = 'Mustang';
+fordMustang.year = 1980;
+fordMustang.price = 100000;
+console.log(fordMustang);
+
+let chevroletCorvette = {
+    color: 'yellow',
+    engine: 'V6 3.2',
+    make: 'Chevrolet',
+    model: 'Corvette',
+    year: 1990,
+    price: 70000
+}
